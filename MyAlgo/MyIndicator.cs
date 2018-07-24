8a19f6fc99ae960c2c19c5f9af033af9ed1204ab
kbank1411 <kbank1411@gmail.com>
2018-07-24 15:07:57 +0900

    add new project

---
+++ MyAlgo/MyIndicator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyAlgo
+{
+    public class MyIndicator
+    {
+        public MyIndicator()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+
+        }
+
+        public static List<string> GetList()
+        {
+            return new List<string>()
+            {
+                "123",
+                "abc",
+                "test"
+            };
+        }
+    }
+}
